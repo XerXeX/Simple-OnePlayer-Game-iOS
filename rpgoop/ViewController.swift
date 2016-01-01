398e7dc5803f0061ba9ef843ea91b611109f6732
Jonatan Knudsen <jonatanknudsen@jonatans-macbook-pro.local>
2016-01-01 18:34:36 +0100

    Initial Commit

---
+++ rpgoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpgoop
+//
+//  Created by Jonatan Knudsen on 01/01/2016.
+//  Copyright © 2016 Jonatan Knudsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
